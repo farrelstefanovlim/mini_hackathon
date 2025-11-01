c0dd18ea1f397bd66eb77a75bb806bf183dd7108
Joenathan <hazendas10@gmail.com>
2025-11-01 08:30:41 +0700

    feat: add authentication routes for login and registration

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+
+Route::middleware(['guest'])->group(function () {
+    Route::view('/login', 'pages.auth.login')->name('login');
+    Route::view('/register', 'pages.auth.register')->name('register');
+
+    Route::post("/login", [AuthController::class, 'login'])->name('login.post');
+    Route::post("/register", [AuthController::class, 'storeUser'])->name('register.post');
+});
 
 Route::get('/', function () {
     return view('welcome');
