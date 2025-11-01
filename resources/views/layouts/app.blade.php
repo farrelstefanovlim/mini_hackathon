c6ac5a10d24c4b790f458737f3c3265e0f7d7d73
riohermanto2428250069 <riohermanto_2428250069@mhs.mdp.ac.id>
2025-11-01 08:36:34 +0700

    layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{config('app.name')}} - @yield('title')</title>
+    <script src="{{asset('js/jquery.js')}}"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @yield("head")
+</head>
+
+<body>
+    <x-navbar/>
+    @yield("body")
+    @yield("script")
+</body>
+</html>
